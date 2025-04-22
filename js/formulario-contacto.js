7a44832a6fbb9c9854c798d4a9d326c2468a704f
WarriorWhite <117951613+OowarriorwhiteoO@users.noreply.github.com>
2025-04-22 15:07:17 -0400

    cambio carpeta contenedora js

--- js/formulario-contacto.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('formulario-contacto').addEventListener('submit', function(event) {
-    //event.preventDefault(); //Evita que la página se recarge
-    
-    alert("Formulario de contacto enviado!")
-  });
